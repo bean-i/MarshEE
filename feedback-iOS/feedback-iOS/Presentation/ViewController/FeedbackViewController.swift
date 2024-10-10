c238a8806b559d63ef5fc063914648a0af8b23f3
Chandrala <gudrms2538@gmail.com>
2024-10-11 02:54:23 +0900

    add: FeedBack 뷰 추가(#7)

---
+++ feedback-iOS/feedback-iOS/Presentation/ViewController/FeedbackViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedbackViewController.swift
+//  feedback-iOS
+//
+//  Created by Chandrala on 10/11/24.
+//
+
+import UIKit
+
+final class FeedbackViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    view.backgroundColor = .systemBlue
+    super.viewDidLoad()
+  }
+}
