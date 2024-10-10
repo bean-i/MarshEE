f97b8188c34a00a222d9bc5d254598e744e71f78
Chandrala <gudrms2538@gmail.com>
2024-10-11 00:01:49 +0900

    feat: TabBarController 구현(#5)

--- feedback-iOS/feedback-iOS/Application/SceneDelegate.swift
+++ feedback-iOS/feedback-iOS/Application/SceneDelegate.swift
@@ -12,17 +12,39 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-      
-      guard let windowScene = (scene as? UIWindowScene) else { return }
-      
-      let window = UIWindow(windowScene: windowScene)
-      let rootViewController = TestViewController()
-      let navigationController = UINavigationController(rootViewController: rootViewController)
-      
-      window.rootViewController = navigationController
-      window.makeKeyAndVisible()
-      
-      self.window = window
-    }
+    
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    let window = UIWindow(windowScene: windowScene)
+    
+    let tabBarController = UITabBarController()
+    
+    let testVC = UINavigationController(rootViewController: TestViewController())
+    testVC.tabBarItem = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
+    
+    let resultVC = UINavigationController(rootViewController: TestViewController())
+    resultVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+    
+    tabBarController.viewControllers = [testVC, resultVC]
+    
+    tabBarController.tabBar.tintColor = .systemBlue
+    
+    let normalAttributes = [
+      NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14),
+      NSAttributedString.Key.foregroundColor: UIColor.gray
+    ]
+    
+    let selectedAttributes = [
+      NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16),
+      NSAttributedString.Key.foregroundColor: UIColor.gray
+    ]
+    
+    UITabBarItem.appearance().setTitleTextAttributes(normalAttributes, for: .normal)
+    UITabBarItem.appearance().setTitleTextAttributes(selectedAttributes, for: .selected)
+    
+    window.rootViewController = tabBarController
+    window.makeKeyAndVisible()
+    
+    self.window = window
+  }
 }
-
