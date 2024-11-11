fbebb22fcf259e99e3d4878f00cf0acdb13a44d8
been <leebin0603@gmail.com>
2024-11-11 14:43:42 +0900

    refactor: HomeViewController 리팩토링

--- feedback-iOS/feedback-iOS/Presentation/ViewController/HomeViewController.swift
+++ feedback-iOS/feedback-iOS/Presentation/ViewController/HomeViewController.swift
@@ -5,102 +5,227 @@
 //  Created by Chandrala on 10/9/24.
 
 import UIKit
-
 import SnapKit
 import Then
 import MultipeerConnectivity
 
 final class HomeViewController: UIViewController {
   
-  let scrollView = UIScrollView()
-  let contentView = UIView()
+  // MARK: - Components
+  private let scrollView = UIScrollView()
+  private let contentView = UIView()
+  private let tapGesture = UITapGestureRecognizer()
   
-  let tapGesture = UITapGestureRecognizer()
+  private let mainImageView = UIImageView()
+  private let mainTitleLabel = UILabel()
+  private let subTitleLabel = UILabel()
   
-  let appearance = UINavigationBarAppearance()
+  private let userInfoHeaderLabel = UILabel()
+  private let userInfoContainerView = UIView()
+  private let userNameLabel = UILabel()
+  private var userNameTextField = UITextField()
+  private let lineView = UIView()
+  private let userDescriptionLabel = UILabel()
+  private var userDescriptionTextField = UITextField()
+  private let userInfoFooterLabel = UILabel()
   
-  let mainImage = UIImageView()
+  private let buttonHeaderLabel = UILabel()
+  private let createButton = UIButton()
+  private let joinButton = UIButton()
+  private let buttonStackView = UIStackView()
+  private let buttonFooterLabel = UILabel()
   
-  let mainLabel = UILabel()
-  let subLabel = UILabel()
-  
-  let userInfoHeader = UILabel()
-  
-  let userInfoTable = UIView()
-  
-  let userNameLabel = UILabel()
-  var userNameTextField = UITextField()
-  
-  let lineView = UIView()
-  
-  let descriptionLabel = UILabel()
-  var descriptionTextField = UITextField()
-  
-  let userInfoFooter = UILabel()
-  
-  let buttonHeader = UILabel()
-  
-  let createButton = UIButton()
-  let joinButton = UIButton()
-  let buttonStackView = UIStackView()
-  
-  let buttonFooter = UILabel()
-  
+  // MARK: - Lifecycle
   public override func viewDidLoad() {
     super.viewDidLoad()
-    
-    setupNotifications()
-    
+    configureTapGesture()
+    configureNotifications()
     setStyle()
     setUI()
     setAutoLayout()
   }
   
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  // MARK: - UI Setup
   private func setStyle() {
+    // View Background Color 적용
     view.backgroundColor = UIColor(red: 242.0 / 255.0, green: 242.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
     
-    title = "홈"
+    // Configure Navigation Bar
+    configureNavigationBar()
     
-    tapGesture.do {
-      $0.addTarget(self, action: #selector(dismissKeyboard))
+    // Configure Sections
+    configureMainSectionStyle()
+    configureUserInfoSectionStyle()
+    configureButtonSectionStyle()
+  }
+  
+  private func setUI() {
+    
+    [createButton, joinButton].forEach {
+      buttonStackView.addArrangedSubview($0)
     }
     
-    appearance.do {
-      $0.configureWithOpaqueBackground()
-      $0.backgroundColor = .white
-      $0.titleTextAttributes = [.foregroundColor: UIColor.black]
+    contentView.addSubviews(
+      mainImageView,
+      mainTitleLabel,
+      subTitleLabel,
+      userInfoHeaderLabel,
+      userInfoContainerView,
+      userNameLabel,
+      userNameTextField,
+      lineView,
+      userDescriptionLabel,
+      userDescriptionTextField,
+      userInfoFooterLabel,
+      buttonHeaderLabel,
+      buttonStackView,
+      buttonFooterLabel
+    )
+    
+    scrollView.addSubview(contentView)
+    view.addSubviews(scrollView)
+    
+  }
+  
+  private func setAutoLayout() {
+    
+    scrollView.snp.makeConstraints {
+      $0.edges.equalTo(view.safeAreaLayoutGuide)
     }
+    
+    contentView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+      $0.height.equalToSuperview()
+      $0.width.equalToSuperview()
+    }
+    
+    mainImageView.snp.makeConstraints {
+      $0.top.equalTo(contentView.snp.top).offset(50)
+      $0.centerX.equalToSuperview()
+      $0.width.height.equalTo(128)
+    }
+    
+    mainTitleLabel.snp.makeConstraints {
+      $0.top.equalTo(mainImageView.snp.bottom).offset(22)
+      $0.centerX.equalToSuperview()
+    }
+    
+    subTitleLabel.snp.makeConstraints {
+      $0.top.equalTo(mainTitleLabel.snp.bottom).offset(22)
+      $0.centerX.equalToSuperview()
+    }
+    
+    userInfoHeaderLabel.snp.makeConstraints {
+      $0.top.equalTo(subTitleLabel.snp.bottom).offset(32)
+      $0.leading.equalToSuperview().inset(32)
+    }
+    
+    userInfoContainerView.snp.makeConstraints {
+      $0.top.equalTo(userInfoHeaderLabel.snp.bottom).offset(10)
+      $0.leading.trailing.equalToSuperview().inset(16)
+      $0.height.equalTo(88)
+      $0.width.equalTo(370)
+    }
+    
+    userNameLabel.snp.makeConstraints {
+      $0.top.equalTo(userInfoContainerView.snp.top).inset(11)
+      $0.leading.equalToSuperview().inset(32)
+    }
+    
+    userNameTextField.snp.makeConstraints {
+      $0.top.equalTo(userInfoContainerView.snp.top).inset(3)
+      $0.bottom.equalTo(lineView.snp.top).inset(-3)
+      $0.leading.equalTo(userNameLabel.snp.trailing).offset(22)
+      $0.trailing.equalToSuperview().inset(32)
+    }
+    
+    lineView.snp.makeConstraints {
+      $0.centerY.equalTo(userInfoContainerView)
+      $0.leading.equalToSuperview().inset(32)
+      $0.trailing.equalTo(userInfoContainerView)
+      $0.height.equalTo(0.33)
+    }
+    
+    userDescriptionLabel.snp.makeConstraints {
+      $0.top.equalTo(lineView.snp.bottom).offset(11)
+      $0.leading.equalToSuperview().inset(32)
+    }
+    
+    userDescriptionTextField.snp.makeConstraints {
+      $0.top.equalTo(lineView.snp.top).inset(3)
+      $0.bottom.equalTo(userInfoContainerView.snp.bottom).inset(3)
+      $0.leading.equalTo(userDescriptionLabel.snp.trailing).offset(22)
+      $0.trailing.equalToSuperview().inset(32)
+    }
+    
+    userInfoFooterLabel.snp.makeConstraints {
+      $0.top.equalTo(userInfoContainerView.snp.bottom).offset(10)
+      $0.leading.equalToSuperview().inset(32)
+    }
+    
+    buttonHeaderLabel.snp.makeConstraints {
+      $0.top.equalTo(userInfoFooterLabel.snp.bottom).offset(22)
+      $0.leading.equalToSuperview().inset(32)
+    }
+    
+    buttonStackView.snp.makeConstraints {
+      $0.top.equalTo(buttonHeaderLabel.snp.bottom).offset(10)
+      $0.leading.trailing.equalToSuperview().inset(16)
+      $0.height.equalTo(60)
+    }
+    
+    buttonFooterLabel.snp.makeConstraints {
+      $0.top.equalTo(buttonStackView.snp.bottom).offset(10)
+      $0.leading.equalToSuperview().inset(32)
+    }
+  }
+  
+  private func configureNavigationBar() {
+    let appearance = UINavigationBarAppearance()
+    appearance.configureWithOpaqueBackground()
+    appearance.backgroundColor = .white
+    appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
     
     navigationController?.navigationBar.prefersLargeTitles = true
     navigationController?.navigationBar.standardAppearance = appearance
     navigationController?.navigationBar.scrollEdgeAppearance = appearance
     
-    mainImage.do {
+    navigationItem.title = "홈"
+  }
+  
+  private func configureMainSectionStyle() {
+    mainImageView.do {
       $0.image = UIImage(named: "chart")
       $0.contentMode = .scaleAspectFit
     }
     
-    mainLabel.do {
+    mainTitleLabel.do {
       $0.text = "당신의 말랑한 SOFT SKILL,\n찌르고 굽고 음미해봐요"
       $0.font = UIFont.sfPro(.title2)
       $0.numberOfLines = 0
       $0.textAlignment = .center
     }
     
-    subLabel.do {
+    subTitleLabel.do {
       $0.text = "의사소통, 자기개발, 문제해결, 팀워크, 리더십을\n상호 피드백으로 발전시켜 보세요."
       $0.font = UIFont.sfPro(.body)
       $0.numberOfLines = 0
       $0.textAlignment = .center
     }
-    
-    userInfoHeader.do {
+  }
+  
+  private func configureUserInfoSectionStyle() {
+    userInfoHeaderLabel.do {
       $0.text = "당신의 아이덴티티"
       $0.font = UIFont.sfPro(.header)
       $0.textColor = .gray
     }
     
-    userInfoTable.do {
+    userInfoContainerView.do {
       $0.backgroundColor = .white
       $0.layer.cornerRadius = 10 //radius
     }
@@ -108,6 +233,7 @@ final class HomeViewController: UIViewController {
     userNameLabel.do {
       $0.text = "이름"
       $0.font = UIFont.sfPro(.body)
+      $0.sizeToFit()
     }
     
     userNameTextField.do {
@@ -119,23 +245,26 @@ final class HomeViewController: UIViewController {
       $0.backgroundColor = UIColor(red: 84.0 / 255.0, green: 84.0 / 255.0, blue: 86.0 / 255.0, alpha: 0.34)
     }
     
-    descriptionLabel.do {
+    userDescriptionLabel.do {
       $0.text = "설명"
       $0.font = UIFont.sfPro(.body)
+      $0.sizeToFit()
     }
     
-    descriptionTextField.do {
+    userDescriptionTextField.do {
       $0.placeholder = "내 역할을 어필해 더 정확한 결과 확인"
       $0.clearButtonMode = .always
     }
     
-    userInfoFooter.do {
+    userInfoFooterLabel.do {
       $0.text = "식별 가능한 유저 이름과 설명을 작성해 주세요"
       $0.font = UIFont.sfPro(.footer)
       $0.textColor = .gray
     }
-    
-    buttonHeader.do {
+  }
+  
+  private func configureButtonSectionStyle() {
+    buttonHeaderLabel.do {
       $0.text = "다음으로 세션을 시작해요"
       $0.font = UIFont.sfPro(.header)
       $0.textColor = .gray
@@ -151,7 +280,7 @@ final class HomeViewController: UIViewController {
       $0.setLayer(borderColor: .clear, cornerRadius: 12)
       $0.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 4)
       $0.titleEdgeInsets = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 0)
-      $0.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+      $0.addTarget(self, action: #selector(createButtonTapped), for: .touchUpInside)
     }
     
     joinButton.do {
@@ -174,7 +303,7 @@ final class HomeViewController: UIViewController {
       $0.distribution = .fillEqually
     }
     
-    buttonFooter.do {
+    buttonFooterLabel.do {
       $0.text = "세션을를 통해 세션을 생성할 수 있고, 이미 생성된 세션에\n참가할 수도 있어요"
       $0.font = UIFont.sfPro(.footer)
       $0.textColor = .gray
@@ -183,137 +312,33 @@ final class HomeViewController: UIViewController {
     }
   }
   
-  private func setUI() {
-    
-    [createButton, joinButton].forEach {
-      buttonStackView.addArrangedSubview($0)
-    }
-    
-    contentView.addSubviews(
-      mainImage,
-      mainLabel,
-      subLabel,
-      userInfoHeader,
-      userInfoTable,
-      userNameLabel,
-      userNameTextField,
-      lineView,
-      descriptionLabel,
-      descriptionTextField,
-      userInfoFooter,
-      buttonHeader,
-      buttonStackView,
-      buttonFooter
-    )
-    
-    scrollView.addSubview(contentView)
-    
-    view.addGestureRecognizer(tapGesture)
-    view.addSubviews(scrollView)
-    
+  // MARK: - Button Actions
+  
+  private func showAlert(title: String, message: String) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    alert.addAction(okAction)
+    present(alert, animated: true, completion: nil)
   }
   
-  private func setAutoLayout() {
-    
-    scrollView.snp.makeConstraints {
-      $0.edges.equalTo(view.safeAreaLayoutGuide)
-    }
-    
-    contentView.snp.makeConstraints {
-      $0.edges.equalToSuperview()
-      $0.width.equalToSuperview()
-      $0.height.equalToSuperview()
-    }
-    
-    mainImage.snp.makeConstraints {
-      $0.width.height.equalTo(128)
-      $0.top.equalTo(contentView.snp.top).offset(50)
-      $0.centerX.equalToSuperview()
-    }
-    
-    mainLabel.snp.makeConstraints {
-      $0.centerX.equalToSuperview()
-      $0.top.equalTo(mainImage.snp.bottom).offset(22)
-    }
-    
-    subLabel.snp.makeConstraints {
-      $0.centerX.equalToSuperview()
-      $0.top.equalTo(mainLabel.snp.bottom).offset(22)
-    }
-    
-    userInfoHeader.snp.makeConstraints {
-      $0.top.equalTo(subLabel.snp.bottom).offset(32)
-      $0.leading.equalToSuperview().inset(32)
-    }
-    
-    userInfoTable.snp.makeConstraints {
-      $0.top.equalTo(userInfoHeader.snp.bottom).offset(10)
-      $0.leading.trailing.equalToSuperview().inset(16)
-      $0.width.equalTo(370)
-      $0.height.equalTo(88)
-    }
-    
-    userNameLabel.snp.makeConstraints {
-      $0.top.equalTo(userInfoTable.snp.top).inset(11)
-      $0.leading.equalToSuperview().inset(32)
-    }
-    
-    userNameTextField.snp.makeConstraints {
-      $0.top.equalTo(userInfoTable.snp.top).inset(11)
-      $0.leading.equalTo(userNameLabel.snp.trailing).offset(22)
-      $0.trailing.equalToSuperview().inset(32)
-      $0.height.equalTo(22)
-    }
-    
-    lineView.snp.makeConstraints {
-      $0.centerY.equalTo(userInfoTable)
-      $0.leading.equalToSuperview().inset(32)
-      $0.trailing.equalTo(userInfoTable)
-      $0.height.equalTo(0.33)
-    }
-    
-    descriptionLabel.snp.makeConstraints {
-      $0.top.equalTo(lineView.snp.bottom).offset(11)
-      $0.leading.equalToSuperview().inset(32)
-    }
-    
-    descriptionTextField.snp.makeConstraints {
-      $0.top.equalTo(lineView.snp.bottom).offset(11)
-      $0.leading.equalTo(descriptionLabel.snp.trailing).offset(22)
-      $0.trailing.equalToSuperview().inset(32)
-      $0.height.equalTo(22)
-    }
-    
-    userInfoFooter.snp.makeConstraints {
-      $0.top.equalTo(userInfoTable.snp.bottom).offset(10)
-      $0.leading.equalToSuperview().inset(32)
-    }
-    
-    buttonHeader.snp.makeConstraints {
-      $0.top.equalTo(userInfoFooter.snp.bottom).offset(22)
-      $0.leading.equalToSuperview().inset(32)
-    }
-    
-    buttonStackView.snp.makeConstraints {
-      $0.top.equalTo(buttonHeader.snp.bottom).offset(10)
-      $0.leading.trailing.equalToSuperview().inset(16)
-      $0.height.equalTo(60)
-    }
-    
-    buttonFooter.snp.makeConstraints {
-      $0.top.equalTo(buttonStackView.snp.bottom).offset(10)
-      $0.leading.equalToSuperview().inset(32)
-    }
-  }
-  
-  func addPaddingToTextField(_ textField: UITextField, padding: CGFloat) {
-    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: textField.frame.height))
-    textField.leftView = paddingView
-    textField.leftViewMode = .always
-  }
-  
-  @objc func showAlert() {
+  @objc func createButtonTapped() {
     print("Create button pressed")
+      
+    // 이름과 역할 작성했는지 체크
+    let userName = userNameTextField.text ?? ""
+    let userDescription = userDescriptionTextField.text ?? ""
+    
+    if userName.isEmpty {
+      showAlert(title: "이름 입력 필요", message: "이름을 작성해주세요.")
+      return
+    }
+    
+    if userDescription.isEmpty {
+      showAlert(title: "역할 입력 필요", message: "역할을 작성해주세요.")
+      return
+    }
+    
+    // 이름과 역할 작성했다면, 프로젝트 생성 창 띄우기
     let alertController = UIAlertController(
       title: "세션 생성하기",
       message: "식별 가능한 프로젝트 이름을 설정해요",
@@ -328,28 +353,30 @@ final class HomeViewController: UIViewController {
     
     let createAction = UIAlertAction(title: "Create", style: .default) { [weak self] _ in
       guard let self = self else { return }
-      
-      let userName = self.userNameTextField.text ?? ""
-      let userRole = self.descriptionTextField.text ?? ""
       let projectName = alertController.textFields?.first?.text ?? ""
+      
+      // 프로젝트 이름 작성 확인
+      if projectName.isEmpty {
+        self.showAlert(title: "프로젝트 이름 필요", message: "프로젝트 이름을 입력해주세요.")
+        return
+      }
+      
+      // 세션 생성
       SessionManager.shared.setLocalUserInfo(
         name: userName,
-        role: userRole
+        role: userDescription
       )
       
-      if !projectName.isEmpty {
-        SessionManager.shared.setSession(
-          isHost: true,
-          displayName: userName,
-          projectName: projectName,
-          delegate: self
-        )
-        
-        let sessionVC = LobbyViewController()
-        self.navigationController?.pushViewController(sessionVC, animated: true)
-      } else {
-        print("프로젝트 이름을 입력해주세요")
-      }
+      SessionManager.shared.setSession(
+        isHost: true,
+        displayName: userName,
+        projectName: projectName,
+        delegate: self
+      )
+      
+      let sessionVC = LobbyViewController()
+      self.navigationController?.pushViewController(sessionVC, animated: true)
+      
     }
     alertController.addAction(cancelAction)
     alertController.addAction(createAction)
@@ -358,53 +385,74 @@ final class HomeViewController: UIViewController {
   }
   
   @objc private func joinButtonTapped() {
-    let userName = self.userNameTextField.text ?? ""
-    let userRole = self.descriptionTextField.text ?? ""
-    print("joinButtonTapped")
+    guard let userName = userNameTextField.text, !userName.isEmpty else {
+      showAlert(title: "이름 입력 필요", message: "이름을 작성해주세요.")
+      return
+    }
+    
+    guard let userDescription = userDescriptionTextField.text, !userDescription.isEmpty else {
+      showAlert(title: "역할 입력 필요", message: "역할을 작성해주세요.")
+      return
+    }
+
     SessionManager.shared.setLocalUserInfo(
       name: userName,
-      role: userRole
+      role: userDescription
     )
     SessionManager.shared.setSession(
       isHost: false,
       displayName: userName,
       delegate: self
     )
-        
-        if let browser = SessionManager.shared.browser {
-          present(browser, animated: true)
-        }
-      }
+    
+    if let browser = SessionManager.shared.browser {
+      present(browser, animated: true)
+    }
+  }
   
-  private func setupNotifications() {
-    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+  // MARK: - Keyboard
+  
+  @objc func dismissKeyboard() {
+    view.endEditing(true)
   }
   
   @objc private func keyboardWillShow(_ notification: Notification) {
     guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
     let keyboardHeight = keyboardFrame.cgRectValue.height
     
-    if userNameTextField.isFirstResponder || descriptionTextField.isFirstResponder {
-      if view.frame.origin.y == 0 {
-        view.frame.origin.y -= keyboardHeight
+    if userNameTextField.isFirstResponder || userDescriptionTextField.isFirstResponder {
+      UIView.animate(withDuration: 0.3) {
+        self.view.frame.origin.y = -keyboardHeight
       }
     }
   }
   
   @objc private func keyboardWillHide(_ notification: Notification) {
-    if view.frame.origin.y != 0 {
-      view.frame.origin.y = 0
+    UIView.animate(withDuration: 0.3) {
+      self.view.frame.origin.y = 0
     }
   }
   
-  @objc func dismissKeyboard() {
-    view.endEditing(true)
+  private func configureTapGesture() {
+    tapGesture.addTarget(self, action: #selector(dismissKeyboard))
+    view.addGestureRecognizer(tapGesture)
   }
   
-  deinit {
-    NotificationCenter.default.removeObserver(self)
+  private func configureNotifications() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillShow(_:)),
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillHide(_:)),
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
   }
+  
 }
 
 // MARK: - MCBrowserViewControllerDelegate
